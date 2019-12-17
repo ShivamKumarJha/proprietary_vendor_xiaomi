afbf54230cecf48cb04b0e02f830ff42a05b37a1
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-18 02:19:45 +0530

    twolip->tulip
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- sdm660-common/Android.mk
+++ sdm660-common/Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout twolip wayne whyred,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout tulip wayne whyred,$(TARGET_DEVICE)),)
 
 endif
